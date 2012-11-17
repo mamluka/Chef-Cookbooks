ad9e7e04f22ff918f50618d0e9a9c546ee56e15c
mamluka <david.mazvovsky@gmail.com>
2012-11-17 17:39:57 +0200

    Update cookbooks/speedymailer-drone/recipes/default.rb

--- cookbooks/speedymailer-drone/recipes/default.rb
+++ cookbooks/speedymailer-drone/recipes/default.rb
@@ -219,7 +219,7 @@ deploy "/deploy/drones" do
         
         execute "run-drone" do
            cwd drone_path
-           command "/usr/bin/mono SpeedyMailer.Drones.exe -s #{node.default["drone"]["master"]} &"
+           command "/usr/bin/mono SpeedyMailer.Drones.exe -s #{node[:drone][:master]} &"
         end
 
     end
